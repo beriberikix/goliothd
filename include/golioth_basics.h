49088bbd70ddb723029f728435638cba14128607
Jonathan Beri <jberi@golioth.io>
2024-11-13 16:15:34 -0800

    first commit

---
+++ include/golioth_basics.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#pragma once
+
+#include <golioth/client.h>
+
+void golioth_basics(struct golioth_client *client);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/main.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <golioth/client.h>
+#include "golioth_basics.h"
+
+#define TAG "main"
+
+int main(void)
+{
+  char *golioth_psk_id = getenv("GOLIOTH_SAMPLE_PSK_ID");
+  if ((!golioth_psk_id) || strlen(golioth_psk_id) <= 0)
+  {
+    fprintf(stderr, "PSK ID is not specified.\n");
+    return 1;
+  }
+  char *golioth_psk = getenv("GOLIOTH_SAMPLE_PSK");
+  if ((!golioth_psk) || strlen(golioth_psk) <= 0)
+  {
+    fprintf(stderr, "PSK is not specified.\n");
+    return 1;
+  }
+
+  struct golioth_client_config config = {
+      .credentials =
+          {
+              .auth_type = GOLIOTH_TLS_AUTH_TYPE_PSK,
+              .psk =
+                  {
+                      .psk_id = golioth_psk_id,
+                      .psk_id_len = strlen(golioth_psk_id),
+                      .psk = golioth_psk,
+                      .psk_len = strlen(golioth_psk),
+                  },
+          },
+  };
+
+  struct golioth_client *client = golioth_client_create(&config);
+  assert(client);
+  golioth_basics(client);
+
+  return 0;
+}
